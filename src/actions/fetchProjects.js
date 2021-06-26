1e7ff431ede2928030367b4709c82d1cd372d8c0
John Madden <jck13mad@gmail.com>
2021-06-26 09:34:50 -0500

    fetch projects set up

---
+++ src/actions/fetchProjects.js
@@ -0,0 +1,8 @@
+export const fetchProjects = () => {
+    return(dispatch) => {
+        dispatch({ type: 'LOADING_PROJECTS' })
+        fetch('http://localhost:3000/api/v1/projects')
+        .then(resp => resp.json())
+        .then(respJSON => dispatch({ type: 'ADD_PROJECTS', projects: respJSON }))
+    }
+}
