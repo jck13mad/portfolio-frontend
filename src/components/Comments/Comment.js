66790737c924a89b4a61a81b8562dcb2b0174525
John Madden <jck13mad@gmail.com>
2021-06-26 11:18:32 -0500

    Comment component filled in

---
+++ src/components/Comments/Comment.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Moment from 'react-moment';
+import Card from 'react-bootstrap/Card';
+
+const Comment = ({ comment }) => {
+    return(
+        <div>
+            <Card 
+                bg='light'
+                style={{ width: '100%' }}
+            >
+                <Card.Header>
+                    <b>{comment.u_name}</b>
+                    <br />
+                    <Moment fromNow>{comment.created_at}</Moment>
+                </Card.Header>
+
+                <Card.Body>
+                    <Card.Text>{comment.content}</Card.Text>
+                </Card.Body>
+            </Card>
+        </div>
+    );
+}
+
+export default Comment
