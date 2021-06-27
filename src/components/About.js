44b3646047e7aa9fe7db49447dc3a6a2ce1b3fe0
John Madden <jck13mad@gmail.com>
2021-06-26 20:42:56 -0500

    Added singular components

---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+    return(
+        <div id='about'>
+            <h1>About</h1>
+            <h3>Full Stack Engineer</h3>
+            <div id='aboutContent'>
+                <p>This is a test for the about section.</p>
+            </div>
+            <div id='skills'>
+                <p>This is a test for the skills section</p>
+            </div>
+        </div>
+    )
+}
+
+export default About 
---
+++ src/components/Projects/ProjectList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Project from './Project'
+
+const ProjectList = ({ projects }) => {
+    const renderProjects = projects.map(project => 
+        <Project key={project.id} project={project} />
+    )
+
+    return(
+        < >
+            {( projects.length > 0) ?
+        renderProjects
+        :
+        <i>There are no matching results.</i>
+      } 
+        </ >
+    )
+}
+
+export default ProjectList
