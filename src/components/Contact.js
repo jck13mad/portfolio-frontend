a3704937c9885fa7f1bea68caefc23914a557743
John Madden <jck13mad@gmail.com>
2021-06-27 08:44:51 -0500

    Outfitted contact component

---
+++ src/components/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react' 
+//add image for each contact  
+
+const Contact = () => {
+    return(
+        <div id='contact'>
+            <h2>CONTACT</h2>
+            <a href="mailto:jck13mad@gmail.com" target="_blank">
+                Email
+            </a>
+        
+        </div>
+    )
+}
