a7ac25579fd456f48a8b477f847bb2a0eb91ab9f
John Madden <jck13mad@gmail.com>
2021-06-27 08:47:28 -0500

    Set up initial home component

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div id='home'>
+                <h1>John Madden</h1>
+                <h4>But you can call me Jack.</h4>
+                <h2>Full-Stack Software Engineer</h2>
+            </div>
+        )
+    }
+}
